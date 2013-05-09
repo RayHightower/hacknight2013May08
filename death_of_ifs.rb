8e97134d3ba2466112cca6858e393f56865f7e75
Raymond T. Hightower <rth@wisdomgroup.com>
2013-05-08 19:49:04 -0500

    working without any ifs

--- death_of_ifs.rb
+++ death_of_ifs.rb
@@ -8,21 +8,45 @@ class QuitCommand
   end
 end
 
+class TweetCommand
+  def match?(input)
+    input == 'tweet'
+  end
 
-def process(input)
-  if input == 'q'
-    puts 'Goodbye'
-  elsif input == 'tweet'
-    puts 'tweeting'
-  elsif input == 'dm'
-    puts 'direct messaging'
-  elsif input == 'help'
-    puts 'helping'
+  def execute
+    puts 'Tweeting things NOW!!!'
   end
 end
 
-j = QuitCommand.new
-puts j.match?('q')
-puts j.match?('8')
+class NoActionCommand
+  def match?(input)
+    true
+  end
 
+  def execute
+    puts 'The NoActionCommand is executing.'
+  end
+end
+
+def process(input)
+  quit = QuitCommand.new
+  tweet = TweetCommand.new
+  no_action = NoActionCommand.new
+
+  commands = [quit, tweet, no_action]
+
+  found_command = commands.find {|command| command.match?(input) }
+  found_command.execute
+
+end
+
+# j = QuitCommand.new
+# puts j.match?('q')
+# puts j.match?('8')
+
+process 'q'
+process 'tweet'
+process 'random'
+process 'whatever'
+process 'catchthat'
 
